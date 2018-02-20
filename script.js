82a85ad085b44fa069e4ea59e7155e39841fbd95
Tyler McNamara <mcnamara14@gmail.com>
2018-02-20 15:01:49 -0700

    Final quality to importance and idea to task cleanup

--- script.js
+++ script.js
@@ -1,63 +1,230 @@
 var $inputTitle = $('.title');
 var $inputBody = $('.input-text');
-var $saveBtn = $('.save-button')
-var $importance = 'importance: Normal';
+var $saveBtn = $('.save-btn')
+var $importance = 'Importance: None';
+var $showCompletedBtn = $('.show-completed');
+var $hideCompletedBtn = $('.hide-completed');
 
-$(window).on('load', prependIdeas);
-$saveBtn.on('click', saveIdea);
+$(window).on('load', prependIncompleteTasks);
+$saveBtn.on('click', saveTask);
 $inputBody.on('keyup', toggleDisableState);
 $inputTitle.on('keyup', toggleDisableState);
-$('.section-ideas').on('click', '.delete', deleteIdeas);
-$('.section-ideas').on('click', '.upvote', upvoteIdea);
-$('.section-ideas').on('click', '.downvote', downvoteIdea);
-$('.section-ideas').on('input', '.idea-title', persistTitle);
-$('.section-ideas').on('input', '.idea-body', persistBody);
-$('.search-field').on('keyup', searchIdeas);
+$showCompletedBtn.on('click', prependCompletedTasks);
+$hideCompletedBtn.on('click', hideCompletedTasks);
+$('.task-list').on('click', '.delete', deleteTasks);
+$('.task-list').on('click', '.upvote', upvoteTask);
+$('.task-list').on('click', '.downvote', downvoteTask);
+$('.task-list').on('input', '.task-title', saveEditedTitle);
+$('.task-list').on('input', '.task-body', saveEditedBody);
+$('.task-list').on('click', '.completed-checkbox', updateCompleted);
+$('.task-list').on('click', '.completed-checkbox', addCompletedClass);
+$('.search-field').on('keyup', search);
 
-function ConstructIdeas(id, title, body, importance) {
+function Task(id, title, body, importance, completed) {
   this.id = id;
   this.title = title;
   this.body = body;
   this.importance = importance;
+  this.completed = '';
+  this.checked = '';
 }
 
-// save idea should be a function, append should be another
-function saveIdea(event) {
+function saveTask(event) {
   event.preventDefault();
-  var newIdea = new ConstructIdeas((jQuery.now()), $inputTitle.val(), $inputBody.val(), $importance)
-  sendToStorage(newIdea);
-  // prependIdea(newIdea);
-  prependIdeas();
-  clearInputs()
+  var newTask = new Task((jQuery.now()), $inputTitle.val(), $inputBody.val(), $importance)
+  sendToStorage(newTask)
+  formReset();
+  clearInputs();
 }
 
-function sendToStorage(idea) {
-  var stringifiedIdea = JSON.stringify(idea);
-  localStorage.setItem(idea.id, stringifiedIdea)
+function sendToStorage(task) {
+  var stringifiedTask = JSON.stringify(task);
+  localStorage.setItem(task.id, stringifiedTask);
+  getFromStorage(task);
 }
 
-// only need one append
-function prependIdeas() {
-  $('.section-ideas').html("");
-  var ideas = [];
+function getFromStorage(task) {
+  var storedTask = JSON.parse(localStorage.getItem(task.id));
+  prependTask(storedTask);
+}
+
+function prependTask(storedTask) {
+    $('.task-list').prepend(`<article class="task-card ${storedTask.completed}" id="${storedTask.id}">
+      <h2 class="task-title" contenteditable="true">${storedTask.title}</h2>
+      <article class="delete" aria-label="Button to delete task"></article>
+      <p class="task-body" contenteditable="true">${storedTask.body}</p>
+      <section class="importance-completed">
+        <article class="upvote"></article>
+        <article class="downvote"></article>
+        <h3 class="importance">${storedTask.importance}</h3>
+        <article class="completed-task">
+          <input type="checkbox" name="completed-task-checkbox" id="completed-checkbox" class="completed-checkbox" value="value" ${storedTask.checked}>
+          <label for="completed-task-checkbox">Completed Task</label>
+        </article>
+        </article>
+      </section>
+      </article>`);
+  }
+
+
+function prependIncompleteTasks() {
+  $('.task-list').html('');
+  var tasks = [];
   var keys = Object.keys(localStorage);
   for (var i = 0; i < keys.length; i++) {
-  var storedIdea = JSON.parse(localStorage.getItem(keys[i]));
-  ideas.push(storedIdea);
-    $('.section-ideas').prepend(`<article class="idea-card" id="${ideas[i].id}">
-      <h2 class="idea-title" contenteditable="true">${ideas[i].title}</h2>
-      <article class="delete" aria-label="Button to delete idea"></article>
-      <p class="idea-body" contenteditable="true">${ideas[i].body}</p>
-      <article class="upvote"></article>
-      <article class="downvote"></article>
-      <h3 class="importance">${ideas[i].importance}</h3>
+  var storedTask = JSON.parse(localStorage.getItem(keys[i]));
+  tasks.push(storedTask);
+  if (tasks[i].completed === '') {
+    $('.task-list').prepend(`<article class="task-card ${tasks[i].completed}" id="${tasks[i].id}">
+      <h2 class="task-title" contenteditable="true">${tasks[i].title}</h2>
+      <article class="delete" aria-label="Button to delete task"></article>
+      <p class="task-body" contenteditable="true">${tasks[i].body}</p>
+      <section class="importance-completed">
+        <article class="upvote"></article>
+        <article class="downvote"></article>
+        <h3 class="importance">${tasks[i].importance}</h3>
+        <article class="completed-task">
+          <input type="checkbox" name="completed-task-checkbox" id="completed-checkbox" class="completed-checkbox" value="value" ${tasks[i].checked}>
+          <label for="completed-task-checkbox">Completed Task</label>
+        </article>
+        </article>
+      </section>
       </article>`);
   }
 }
 
- function formReset(){
-    clearInputs();
-    toggleDisableState();
+}
+
+function prependCompletedTasks() {
+  var tasks = [];
+  var keys = Object.keys(localStorage);
+  for (var i = 0; i < keys.length; i++) {
+  var storedTask = JSON.parse(localStorage.getItem(keys[i]));
+  tasks.push(storedTask);
+  if (tasks[i].completed === 'completed') {
+    $('.task-list').prepend(`<article class="task-card ${tasks[i].completed}" id="${tasks[i].id}">
+      <h2 class="task-title" contenteditable="true">${tasks[i].title}</h2>
+      <article class="delete" aria-label="Button to delete task"></article>
+      <p class="task-body" contenteditable="true">${tasks[i].body}</p>
+      <section class="importance-completed">
+        <article class="upvote"></article>
+        <article class="downvote"></article>
+        <h3 class="importance">${tasks[i].importance}</h3>
+        <article class="completed-task">
+          <input type="checkbox" name="completed-task-checkbox" id="completed-checkbox" class="completed-checkbox" value="value" ${tasks[i].checked}>
+          <label for="completed-task-checkbox">Completed Task</label>
+        </article>
+        </article>
+      </section>
+      </article>`);
+    }
+  }
+  toggleCompletedBtn();
+}
+
+function toggleCompletedBtn() {
+  var showHideBtnText = $('.show-hide-btn').text();
+  if (showHideBtnText === 'Show Completed') {
+    $('.show-hide-btn').text('Hide Completed').removeClass('show-completed').addClass('hide-completed');
+  } else {
+     hideCompletedTasks();
+  }
+}
+
+function hideCompletedTasks() {
+  $('.completed').addClass('hide');
+  $('.show-hide-btn').text('Show Completed').removeClass('hide-completed').addClass('show-completed');
+}
+
+function deleteTasks() {
+  $(this).closest('.task-card').fadeOut();
+  var id = $(this).closest('.task-card').attr('id');
+  localStorage.removeItem(id);
+}
+
+function upvoteTask() {
+  if ($(this).siblings('h3').text() === 'Importance: None') {
+    $(this).siblings('h3').text('Importance: Low');
+  } else if ($(this).siblings('h3').text() === 'Importance: Low') {
+    $(this).siblings('h3').text('Importance: Normal');
+  } else if ($(this).siblings('h3').text() === 'Importance: Normal') {
+    $(this).siblings('h3').text('Importance: High');
+  } else {
+    $(this).siblings('h3').text('Importance: Critical');
+  }
+  saveImportance(this)
+}
+
+function downvoteTask() {
+  if ($(this).siblings('h3').text() === 'Importance: Critical') {
+    $(this).siblings('h3').text('Importance: High');
+  } else if ($(this).siblings('h3').text() === 'Importance: High') {
+    $(this).siblings('h3').text('Importance: Normal');
+  } else if ($(this).siblings('h3').text() === 'Importance: Normal') {
+    $(this).siblings('h3').text('Importance: Low');
+  } else {
+    $(this).siblings('h3').text('Importance: None');
+  }
+  saveImportance(this)
+}
+
+function saveImportance(newThis) {
+  var key = $(newThis).closest('.task-card').attr('id');
+  var stringifiedTask = localStorage.getItem(key);
+  var parsedTask = JSON.parse(stringifiedTask);
+  parsedTask.importance = $(newThis).siblings('h3').text();
+  var stringifiedTask = JSON.stringify(parsedTask)
+  localStorage.setItem(key, stringifiedTask);
+}
+
+function saveEditedTitle() {
+  var key = $(this).closest('.task-card').attr('id');
+  var stringifiedTask = localStorage.getItem(key);
+  task = JSON.parse(stringifiedTask);
+  task.title = $(this).text();
+  var changedTask = JSON.stringify(task)
+  localStorage.setItem(key, changedTask);
+}
+
+function saveEditedBody() {
+  var key = $(this).closest('.task-card').attr('id');
+  var stringifiedTask = localStorage.getItem(key);
+  task = JSON.parse(stringifiedTask);
+  task.body = $(this).text();
+  var changedTask = JSON.stringify(task)
+  localStorage.setItem(key, changedTask);
+}
+
+function search() {
+  var $input = $('.search-field').val().toLowerCase();
+  $(".task-card").filter(function() {
+  $(this).toggle($(this).text().toLowerCase().indexOf($input) > -1)
+  }); 
+}
+
+
+function updateCompleted() {
+  var key = $(this).closest('.task-card').attr('id');
+  var stringifiedTask = localStorage.getItem(key);
+  task = JSON.parse(stringifiedTask);
+  if (task.completed === '') {
+    task.completed = 'completed';
+    task.checked = 'checked'; 
+  } else {
+    task.completed = '';
+    task.checked = '';
+  }
+  var changedTask = JSON.stringify(task);
+  localStorage.setItem(key, changedTask);
+}
+
+function addCompletedClass() {
+  $(this).closest('.task-card').toggleClass('completed');
+}
+
+function formReset(){
+  clearInputs();
+  toggleDisableState();
 }
 
 function clearInputs() {
@@ -66,109 +233,11 @@ function clearInputs() {
   $inputTitle.focus();
 }
 
-// need to grey out when disabled
 function toggleDisableState() {
   if ($inputBody.val() && $inputTitle.val()) {
     $saveBtn.prop('disabled', false);
   } else {
     $saveBtn.prop('disabled', true);
-  }
-}
-
-function deleteIdeas() {
-  $(this).closest('.idea-card').fadeOut();
-  var id = $(this).closest('.idea-card').attr('id');
-  localStorage.removeItem(id);
-}
-
-// change names
-function saveImportance() {
-  var key = $(newthis).closest('.idea-card').attr('id');
-  var stringifiedIdea = localStorage.getItem(key);
-  var parsedIdea = JSON.parse(stringifiedIdea);
-  idea.importance = $(newthis).siblings('h3').text();
-  var stringifiedIdea = JSON.stringify(idea)
-  localStorage.setItem(id, stringifiedIdea);
-}
-
-// should be able to grab class .importance instead of h3
-function upvoteIdea() {
-  if ($(this).siblings('h3').text() === 'importance: None') {
-    $(this).siblings('h3').text('importance: Low');
-  } else if ($(this).siblings('h3').text() === 'importance: Low') {
-    $(this).siblings('h3').text('importance: Normal');
-  } else if ($(this).siblings('h3').text() === 'importance: Normal') {
-    $(this).siblings('h3').text('importance: High');
-  } else if ($(this).siblings('h3').text() === 'importance: High') {
-    $(this).siblings('h3').text('importance: Critical');
-  }
-  saveImportance(this)
-}
-
-function downvoteIdea() {
-  if ($(this).siblings('h3').text() === 'importance: Critical') {
-    $(this).siblings('h3').text('importance: High')
-  } else if ($(this).siblings('h3').text() === 'importance: High') {
-    $(this).siblings('h3').text('importance: Normal')
-  } else if ($(this).siblings('h3').text() === 'importance: Normal') {
-    $(this).siblings('h3').text('importance: Low')
-  } else if ($(this).siblings('h3').text() === 'importance: Low') {
-    $(this).siblings('h3').text('importance: None')
-  }
-  saveImportance(this)
-}
-
-// change names
-// not saving the last character
-// not sure what e.keyode is doing
-function persistTitle(e) {
-  if (e.keyCode === 13) {
-    e.preventDefault();
-    $inputTitle.focus();
-  }
-  var id = $(this).closest('.idea-card').attr('id');
-  var idea = localStorage.getItem(id);
-  idea = JSON.parse(idea);
-  idea.title = $(this).text();
-  var stringifiedIdea = JSON.stringify(idea)
-  localStorage.setItem(id, stringifiedIdea);
-}
-
-// change names
-// not saving the last character
-// not sure what e.keyode is doing
-function persistBody(e) {
-  if (e.keyCode === 13) {
-  e.preventDefault();
-  $inputTitle.focus();
-  }
-  var id = $(this).closest('.idea-card').attr('id');
-  var idea = localStorage.getItem(id);
-  idea = JSON.parse(idea);
-  idea.body = $(this).text();
-  var stringifiedIdea = JSON.stringify(idea)
-  localStorage.setItem(id, stringifiedIdea);
-}
-
-// should only need to call search for entire idea
-// not saving the last character
-function searchIdeas() {
-   $('.idea-card').hide();
-  search('.importance');
-  search('.idea-body');
-  search('.idea-title');
-
-}
-
-function search(selector) {
-  console.log(selector)
-  var $input = $('.search-field').val();
-  $input = $input.toUpperCase();
-  var array = $(selector);
-  for (var i = 0; i < array.length; i++) {
-    if ($(array[i]).text().toUpperCase().includes($input)) {
-      $(array[i]).closest('article').show();
-    }
   }
 }
 
